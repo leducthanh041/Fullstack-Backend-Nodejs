5daa0ece76a4e33fbdce0d3010c249436189026b
leducthanh041 <leducthanh041282@gmail.com>
2025-08-06 21:44:37 +0700

    #19 DONe ENV(Environment Variables)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
+
+// console.log(">>> check env: ", process.env);
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 // Config template engin
 app.set("views", path.join(__dirname, "views"));
