4a15ed96a6e7f1ea2bdf982c8774ecde09e2c050
leducthanh041 <leducthanh041282@gmail.com>
2025-08-08 16:45:13 +0700

    #30 Using MySQL with Node.JS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
+const mysql = require('mysql2');
 require("dotenv").config();
 const express = require("express");
-
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 
@@ -13,6 +13,25 @@ configViewEngine(app);
 
 // route declaration
 app.use("/", webRoutes);
+
+// test connection
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+// A simple SELECT query
+connection.query(
+  'SELECT * FROM Users u;',
+  function (err, results, fields) {
+    console.log(">>> results= ", results); // results contains rows returned by server
+    console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
