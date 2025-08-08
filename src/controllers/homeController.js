ca65f1f65bfe53bab4ecc4a932dddbc237253354
leducthanh041 <leducthanh041282@gmail.com>
2025-08-08 19:35:07 +0700

    #35 Design NavBar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,7 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  // process data
-  // call model
-  users = [];
-  connection.query("SELECT * FROM Users u;", function (err, results, fields) {
-    users = results;
-    console.log(">>> results= ", results); // results contains rows returned by server
-
-    console.log(">> check users: ", users);
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 
 const getHoiDanIT = (req, res) => {
