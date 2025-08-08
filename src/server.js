712536da363d03ae1192d79725646c4c683594d1
leducthanh041 <leducthanh041282@gmail.com>
2025-08-08 16:53:57 +0700

    #31 Reuse connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql2');
 require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const connection = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -15,23 +15,12 @@ configViewEngine(app);
 app.use("/", webRoutes);
 
 // test connection
-// create the connection to database
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "123456",
-  database: "hoidanit",
-});
 
 // A simple SELECT query
-connection.query(
-  'SELECT * FROM Users u;',
-  function (err, results, fields) {
-    console.log(">>> results= ", results); // results contains rows returned by server
-    console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
-  }
-);
+connection.query("SELECT * FROM Users u;", function (err, results, fields) {
+  console.log(">>> results= ", results); // results contains rows returned by server
+  console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
