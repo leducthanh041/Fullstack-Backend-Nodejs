2043dd9f9beecc55d7dd096a2f04b82e5e605f2e
leducthanh041 <leducthanh041282@gmail.com>
2025-08-08 20:31:28 +0700

    #38 Write data to database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,8 +9,22 @@ const getHoiDanIT = (req, res) => {
 };
 
 const postCreateUser = (req, res) => {
-  console.log(">>> req.body: ", req.body);
-  res.send("create a new user");
+  let email = req.body.email;
+  let name = req.body.myName;
+  let city = req.body.city;
+
+  console.log(">>> email= ", email, "name =", name, "city =", city);
+
+  connection.query(
+    `INSERT INTO Users (email, name, city) 
+    VALUES (?, ?, ?)`,
+    [email, name, city],
+    function (err, results) {
+      console.log(results);
+
+      res.send("Created user succeed!");
+    }
+  );
 };
 
 module.exports = {
