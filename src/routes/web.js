9495e109f12707b72703e7e52b46f932d1faf820
leducthanh041 <leducthanh041282@gmail.com>
2025-08-09 19:49:31 +0700

    #44 Get User By Ids

--- src/routes/web.js
+++ src/routes/web.js
@@ -12,7 +12,7 @@ router.get("/", getHomePage);
 router.get("/hoidanit", getHoiDanIT);
 
 router.get("/create", getCreatePage);
-router.get("/update", getUpdatePage);
+router.get("/update/:id", getUpdatePage);
 
 router.post("/create-user", postCreateUser);
 
